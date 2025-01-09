d3300c5ccc3d48d723c4d7f0bde3bd73a3be738e
Aravinth-herdx <aravinthr@pro17analytics.com>
2025-01-09 22:14:14 +0530

    fix the variant image

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -10,18 +10,18 @@ export const validateProduct = (productData) => {
       errors.push('Regular price is required');
     }
   
-    if (productData.variants?.length > 0) {
-      const skus = new Set();
-      productData.variants.forEach(variant => {
-        if (!variant.sku) {
-          errors.push('SKU is required for all variants');
-        }
-        if (skus.has(variant.sku)) {
-          errors.push('Duplicate SKU found in variants');
-        }
-        skus.add(variant.sku);
-      });
-    }
+    // if (productData.variants?.length > 0) {
+    //   const skus = new Set();
+    //   productData.variants.forEach(variant => {
+    //     if (!variant.sku) {
+    //       errors.push('SKU is required for all variants');
+    //     }
+    //     if (skus.has(variant.sku)) {
+    //       errors.push('Duplicate SKU found in variants');
+    //     }
+    //     skus.add(variant.sku);
+    //   });
+    // }
   
     return {
       isValid: errors.length === 0,
