14c94730584a0a24049e47e1559215628fb9cc50
Aravinth-herdx <aravinthr@pro17analytics.com>
2025-03-05 11:02:54 +0530

    Implement api for watchlater and add to playlist

---
+++ src/model/playlistModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const PlaylistSchema = new mongoose.Schema({
+  playlistId: { type: String, unique: true, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  videos: [{ type: String }], // Array of video IDs
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Playlist", PlaylistSchema);
