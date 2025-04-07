7afeb0dbd7837761128defc95c1eafcd209bad7f
Aravinth-herdx <aravinthr@pro17analytics.com>
2025-04-07 20:41:39 +0530

    api for Link business Account to user

--- src/router/linkAccountRouters.js
+++ src/router/linkAccountRouters.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getLinkedAccounts, fetchAllUsers, linkAccount, confirmLink, rejectLink } from "../controller/linkAccountController.js";
+import { getLinkedAccounts, fetchAllUsers, linkAccount, confirmLink, rejectLink,updateLinkStatus } from "../controller/linkAccountController.js";
 
 const router = express.Router();
 
 router.get("/getLinkedAccounts", getLinkedAccounts);
 router.get("/fetchAllUsers", fetchAllUsers);
+router.post("/updateLinkStatus", updateLinkStatus);
 router.post("/linkAccount", linkAccount);
 router.post("/confirmLink", confirmLink);
 router.post("/rejectLink", rejectLink);
--- src/router/notificationRoutes.js
+++ src/router/notificationRoutes.js
@@ -1,23 +1,28 @@
 import express from 'express';
-import { registerDeviceToken } from '../service/pushNotificationService.js';
-import { handleSuccess, handleError } from '../utils/responseHandler.js';
+// import { registerDeviceToken } from '../service/pushNotificationService.js';
+// import { handleSuccess, handleError } from '../utils/responseHandler.js';
+import { getNotifications } from "../controller/notificationController.js";
+
 
 const router = express.Router();
 
+
+router.get("/getNotifications", getNotifications);
+
 // Register device token
-router.post('/register-device', async (req, res) => {
-  try {
-    const { user_id, device_token, device_type } = req.body;
+// router.post('/register-device', async (req, res) => {
+//   try {
+//     const { user_id, device_token, device_type } = req.body;
     
-    if (!user_id || !device_token || !device_type) {
-      return handleError(res, 400, 'Missing required fields');
-    }
+//     if (!user_id || !device_token || !device_type) {
+//       return handleError(res, 400, 'Missing required fields');
+//     }
     
-    await registerDeviceToken(user_id, device_token, device_type);
-    return handleSuccess(res, 200, 'Device token registered successfully');
-  } catch (error) {
-    return handleError(res, 500, `Error registering device token: ${error.message}`);
-  }
-});
+//     await registerDeviceToken(user_id, device_token, device_type);
+//     return handleSuccess(res, 200, 'Device token registered successfully');
+//   } catch (error) {
+//     return handleError(res, 500, `Error registering device token: ${error.message}`);
+//   }
+// });
 
 export default router;
