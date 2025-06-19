0564cf0281ec7940b03566010cb8ccc59dfdbef9
Jenirathinam <jenijenifer1511@gmail.com>
2025-06-19 12:39:05 +0530

    add postType

--- src/model/createPostModel.js
+++ src/model/createPostModel.js
@@ -92,7 +92,7 @@ const createPostSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   userAvatar: { type: String },
   productId: { type: String },
-  postType: { type: String, required: true },
+  postType: { type: String, required: false },
 
   likesCount: { type: Number, default: 0 },
   commentsCount: { type: Number, default: 0 },
